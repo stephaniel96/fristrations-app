34fe21437db44000f8fd3631a8c1019b4cbf5b0a
stephaniel96 <stephaniel96@gmail.com>
2016-03-24 17:14:50 -0400

    Tabbed applicatoin

---
+++ Fristrations/Fristrations/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fristrations
+//
+//  Created by Stephanie Liu on 3/24/16.
+//  Copyright © 2016 Stephanie Liu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
