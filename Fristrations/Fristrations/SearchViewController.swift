caefb9504e3522d9cb683efcc24b41a01923eb3c
Christina Huang <xuedonghuang@nat-oitwireless-inside-vapornet100-d-3619.Princeton.EDU>
2016-04-16 21:29:46 -0400

    military current time

--- Fristrations/Fristrations/SearchViewController.swift
+++ Fristrations/Fristrations/SearchViewController.swift
@@ -7,21 +7,42 @@
 //
 
 import UIKit
+import Firebase
 
 class SearchViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: Properties
     
-    
-    
+    var rootRef: Firebase!
+    var currentTime:String
+    var roomURL:String = "https://fristrations.firebaseio.com/rooms"
+    let rooms = [114, 205, 206, 207, 208, 209, 210, 212, 227, 228, 234, 303, 307, 309]
+    var availableRooms:[String] = []
     // Called when the view controller’s content view is created and loaded from a storyboard
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        self.title = "Search"
+        self.title = "Available Rooms"
         // Fristrations color in RGB percentages
         view.backgroundColor = UIColor(red: 0.62, green: 0.773, blue: 0.843, alpha: 1.0)
+
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Minute , .Hour], fromDate: date)
+        let minute =  components.minute
+        let hour = components.hour
+        
+        currentTime = String(hour) + String(minute)
+        
+        
+        
+        
+        //for number in rooms {
+       //     roomRef = Firebase(url:(roomURL + roomNumber))
+       //     if
+       // }
         
     }
     
