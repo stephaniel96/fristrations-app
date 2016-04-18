cd02e1fdb68c9287c88dc555c8f6620cf42632bb
Christina Huang <xuedonghuang@nat-oitwireless-inside-vapornet100-d-3619.Princeton.EDU>
2016-04-18 14:30:04 -0400

    shows all available rooms

--- Fristrations/Fristrations/SearchViewController.swift
+++ Fristrations/Fristrations/SearchViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class SearchViewController: UIViewController, UITextFieldDelegate {
+class SearchViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource{
     
     // MARK: Properties
     
@@ -27,6 +27,8 @@ class SearchViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Available Rooms"
+        tableView.delegate = self
+        tableView.dataSource = self
         // Fristrations color in RGB percentages
         view.backgroundColor = UIColor(red: 0.62, green: 0.773, blue: 0.843, alpha: 1.0)
     }
@@ -60,7 +62,7 @@ class SearchViewController: UIViewController, UITextFieldDelegate {
                 let timeDetails = self.times[self.currentTime] as! String
                 if (timeDetails == "n/a") {
                     self.availableRooms.append(roomNumber)
-                    print(self.availableRooms)
+                    self.tableView.reloadData()
                 }
                 
             })
@@ -71,8 +73,15 @@ class SearchViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return availableRooms.count
+    }
     
-    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Customcell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = availableRooms[indexPath.item]
+        return cell
+    }
     /*
     // MARK: - Navigation
     
