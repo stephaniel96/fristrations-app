cb6ef617376e6cfe486199d93e61db3735da6358
stephaniel96 <stephaniel96@gmail.com>
2016-04-19 21:52:21 -0400

    cleaned up code, added custom cell file

--- Fristrations/Fristrations/SearchViewController.swift
+++ Fristrations/Fristrations/SearchViewController.swift
@@ -78,18 +78,10 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UITableViewDe
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Customcell", forIndexPath: indexPath) as! UITableViewCell
-        cell.textLabel?.text = availableRooms[indexPath.item]
+        let cell = tableView.dequeueReusableCellWithIdentifier("Customcell", forIndexPath: indexPath) as! CustomCell
+        cell.roomLabel.text = availableRooms[indexPath.item]
         return cell
     }
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
+
     
 }
