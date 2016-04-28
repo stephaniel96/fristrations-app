4c6a93655398ab9855d60eaa42647b6fb6e2420e
Chris Hsu <chrishsu909@gmail.com>
2016-04-28 16:11:56 -0400

    changed Podfile

---
+++ Fristrations/Pods/Target Support Files/Pods-FristrationsUITests/Pods-FristrationsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FristrationsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FristrationsUITests
+@end
--- Fristrations/Pods/Target Support Files/Pods-FristrationsUITests/Pods-FristrationsUITests-frameworks.sh
+++ Fristrations/Pods/Target Support Files/Pods-FristrationsUITests/Pods-FristrationsUITests-frameworks.sh
@@ -82,14 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Firebase/Firebase.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/HTMLReader/HTMLReader.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Kanna/Kanna.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Firebase/Firebase.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/HTMLReader/HTMLReader.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Kanna/Kanna.framework"
-fi
