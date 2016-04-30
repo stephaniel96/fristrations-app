4502e109e4bf47bcd2fdb24c1e467596bd6c1037
Chris Hsu <chrishsu909@gmail.com>
2016-04-30 15:06:50 -0400

    sign in on more

--- Fristrations/Fristrations/MoreViewController.swift
+++ Fristrations/Fristrations/MoreViewController.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import Kanna
+import Foundation
 
-class MoreViewController: UITableViewController {
+class MoreViewController: UITableViewController,  UIWebViewDelegate{
+    
+    
+    
+    @IBOutlet weak var signInStatus: UILabel!
+    @IBOutlet weak var signInButton: UILabel!
+    
+    var casV: UIWebView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +27,18 @@ class MoreViewController: UITableViewController {
         self.title = "Settings"
         // Fristrations color in RGB percentages
         view.backgroundColor = UIColor(red: 0.62, green: 0.773, blue: 0.843, alpha: 1.0)
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        if (uName == "n/a") {
+            signInStatus.text = "No User Signed In"
+            signInButton.text = "Sign In"
+        }
+        else {
+            signInStatus.text = "Signed in as: \(uName)"
+            signInButton.text = "Sign Out"
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -37,12 +59,71 @@ class MoreViewController: UITableViewController {
      */
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        var alert = UIAlertView()
-        alert.delegate = self
-        alert.title = "Selected Row"
-        alert.message = "You selected row \(indexPath.row)"
-        alert.addButtonWithTitle("OK")
-        alert.show()
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        if (indexPath.section == 0 && indexPath.row == 1) {
+            
+            let netID: AnyObject? = NSUserDefaults.standardUserDefaults().objectForKey("netid")
+            if (netID == nil) {
+                casV = UIWebView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height))
+                casV.loadRequest(NSURLRequest(URL: NSURL(string: "https://www.cs.princeton.edu/~cjhsu/fristrations/CASlogin.php")!))
+                casV.delegate = self
+                casV.layer.zPosition = 1
+                self.view.addSubview(casV)
+                signInButton.text = "Sign In"
+            }
+            else {
+                signInButton.text = "Sign In"
+                NSUserDefaults.standardUserDefaults().removeObjectForKey("netid")
+                casV = UIWebView(frame: CGRectMake(0, 64, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height))
+                casV.loadRequest(NSURLRequest(URL: NSURL(string: "https://fed.princeton.edu/cas/logout")!))
+                casV.delegate = self;
+                signInStatus.text = "Not Signed In"
+                uName = "n/a"
+            }
+        }
+    }
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
+        print("Webview fail with error \(error)");
+    }
+    
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        print("Webview started Loading")
+    }
+    
+    func deleteAnimationComplete(value: Bool)
+    {
+        if (value && casV != nil)
+        {
+            casV.removeFromSuperview()
+            casV = nil
+        }
+        
+    }
+    
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        let docPage = webView.stringByEvaluatingJavaScriptFromString("document.documentElement.outerHTML")!
+        
+        if let doc = Kanna.HTML(html: docPage, encoding: NSUTF8StringEncoding) {
+            // Search for nodes by CSS
+            let bods = doc.css("body")
+            let bod = bods[0].text
+            if (bod!.characters.count < 100) {
+                let netID = bod!.stringByTrimmingCharactersInSet(
+                    NSCharacterSet.whitespaceAndNewlineCharacterSet()
+                )
+                print("login successful!")
+                uName = netID
+                signInStatus.text = "Logged in as: " + uName
+                signInButton.text = "Sign Out"
+                UIView.animateWithDuration(0.5, animations: {self.casV!.alpha = 0}, completion: deleteAnimationComplete)
+                NSUserDefaults.standardUserDefaults().setObject(netID, forKey: "netid")
+            }
+        }
+        
     }
     
 }
