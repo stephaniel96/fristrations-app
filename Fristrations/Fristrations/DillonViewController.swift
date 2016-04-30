a79bf44119117288a3745a64657a2006d670b15d
stephaniel96 <stephaniel96@gmail.com>
2016-04-30 16:27:20 -0400

    dillon page label

--- Fristrations/Fristrations/DillonViewController.swift
+++ Fristrations/Fristrations/DillonViewController.swift
@@ -10,21 +10,23 @@ import UIKit
 import Firebase
 
 class DillonViewController: UIViewController {
+    @IBOutlet weak var dillonLabel: UILabel!
     
     var dillonURL:String = "https://fristrations.firebaseio.com/dillon_gym/num_devices"
     var dillonRef: Firebase!
-    var population:Int = 0
+    var population:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Settings"
+        self.title = "Dillon Gym"
         // Fristrations color in RGB percentages
         view.backgroundColor = UIColor(red: 0.62, green: 0.773, blue: 0.843, alpha: 1.0)
         
         let dillonRouter = Firebase(url:dillonURL)
         dillonRouter.observeEventType(.Value, withBlock: {
             snapshot in
-            self.population = Int(snapshot.value as! String)!
+            self.population = snapshot.value as! String
+            self.dillonLabel.text = "There are currently " + self.population + " devices connected"
         
         })
 
