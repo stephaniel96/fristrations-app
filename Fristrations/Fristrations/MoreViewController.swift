ee85ecf178e26c0943d4dc98887fcb4990f0bba1
Kevin Bradicich <kevin.bradicich@gmail.com>
2016-04-30 13:45:42 -0400

    pushing for chris

--- Fristrations/Fristrations/MoreViewController.swift
+++ Fristrations/Fristrations/MoreViewController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class MoreViewController: UIViewController{
+class MoreViewController: UIViewController, UITableViewDelegate {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        self.title = "Settings"
+        // Fristrations color in RGB percentages
+        view.backgroundColor = UIColor(red: 0.62, green: 0.773, blue: 0.843, alpha: 1.0)
+    }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
+//    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//         let indexPath = tableView.indexPathForSelectedRow()
+//    }
     
+    // testing push
     
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
 }
-
