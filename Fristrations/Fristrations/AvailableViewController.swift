6cc05a565d7becfed923f981de03a42ec8529862
Mihika Kapoor <mkapoor@princeton.edu>
2016-05-03 01:54:16 -0400

    available room labels

--- Fristrations/Fristrations/AvailableViewController.swift
+++ Fristrations/Fristrations/AvailableViewController.swift
@@ -22,20 +22,20 @@ class AvailableViewController: UIViewController, UITextFieldDelegate, UITableVie
     var roomURL:String = "https://fristrations.firebaseio.com/rooms/"
     let rooms = ["frist114", "frist205", "frist206", "frist207", "frist208", "frist209", "frist210", "frist212", "frist227", "frist228", "frist234", "frist303", "frist307", "frist309"]
     let displayRoom =
-        ["frist114" : "Frist 114",
-         "frist205" : "Frist 205",
-         "frist206" : "Frist 206",
-         "frist207" : "Frist 207",
-         "frist208" : "Frist 208",
-         "frist209" : "Frist 209",
-         "frist210" : "Frist 210",
-         "frist212" : "Frist 212",
-         "frist227" : "Frist 227",
-         "frist228" : "Frist 228",
-         "frist234" : "Frist 234",
-         "frist303" : "Frist 303",
-         "frist307" : "Frist 307",
-         "frist309" : "Frist 309"]
+        ["frist114" : "114",
+         "frist205" : "205",
+         "frist206" : "206",
+         "frist207" : "207",
+         "frist208" : "208",
+         "frist209" : "209",
+         "frist210" : "210",
+         "frist212" : "212",
+         "frist227" : "227",
+         "frist228" : "228",
+         "frist234" : "234",
+         "frist303" : "303",
+         "frist307" : "307",
+         "frist309" : "309"]
     let revDisplayRoom =
         ["Frist 114" : "frist114",
          "Frist 205" : "frist205",
